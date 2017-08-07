5b96f2012725f05aefb137fbfcd2cff10ba21e6c
FGE-HP\FGE <rexar1130@live.com.mx>
2017-08-07 10:19:07 -0500

    login en desaparecidos

--- desaparecidos/login.php
+++ desaparecidos/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Gobierno Abierto - Registro Público de Personas Detenidas</title>
+    <title>Gobierno Abierto - Registro Público de Personas Desaparecidas</title>
     <link rel="icon" href="img/icon.png" sizes="192x192">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
