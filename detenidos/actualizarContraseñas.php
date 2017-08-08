16c8f7d6f8f09433115ff1b630abcfc9fa006019
FGE-HP\FGE <wolverinenain@gmail.com>
2017-08-08 13:55:44 -0500

    Archivo para actualizar contraseñas

---
+++ detenidos/actualizarContraseñas.php
@@ -0,0 +1,15 @@
+<?php
+$conexion = new mysqli('localhost', 'root', '', 'detenidos');
+$sql = " SELECT id, password FROM db_users";
+$resultado = $conexion->query($sql);
+
+if( $resultado->num_rows > 0 ){
+	while($usuario = $resultado->fetch_assoc()){
+		$password = $usuario['password'];
+		echo $usuario['password']."---------";
+		$hash = password_hash($password, PASSWORD_BCRYPT, array("cost" => 10));
+		$sql = "UPDATE db_users SET password = '".$hash."' WHERE id = ".$usuario['id'];
+		$resultado2 = $conexion->query($sql);
+		echo $hash."<br>";
+	}
+}
