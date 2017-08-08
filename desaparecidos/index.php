e28958fb5c94fcd9907b79ced036f588f9d52c76
Arturo Guillen <arturo.gja@hotmail.com>
2017-08-08 16:31:15 -0500

    Mensajes Actualizados

--- desaparecidos/index.php
+++ desaparecidos/index.php
@@ -183,6 +183,31 @@ $app->post('/updatePassword', function () use ($app, $logger,$dao) {
       echo json_encode($response, JSON_UNESCAPED_UNICODE);
 });
 
+$app->post('/enviaReporte', function () use ($app, $logger,$dao) {
+    $app->response()->header('Content-Type', 'application/json; charset=utf-8');
+    $response = array();
+    $request =$app->request()->getBody()!=='' ? json_decode($app->request()->getBody()): null;    
+    try{
+        $request = json_decode($app->request()->getBody());
+        $fechaInicial = !empty($request->fechaInicial)?$request->fechaInicial:NULL;
+        $fechaFinal = !empty($request->fechaFinal)?$request->fechaFinal:NULL;
+        $registros = $dao->enviaReporte($fechaInicial, $fechaFinal, $_SESSION['idUsuario'], $_SESSION['userLevel']);
+        echo $registros;
+        if (!$registros) {
+            throw new Exception('No existe registro', 418);
+        }
+        else{
+            return;
+        }
+    } catch (Exception $e) {
+        $response['error_code'] = $e->getCode();
+        $response['error_message'] = $e->getMessage();
+        $logger->debug('enviaReporte: ' . $e->getMessage());
+
+    }
+      echo json_encode($response, JSON_UNESCAPED_UNICODE);
+});
+
 
 $app->run();
 ?>
